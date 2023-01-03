53b8db8f8978a926cb4a94f33defd1e59ff3feb1
yorkmacbook060 <marquisgerrard@gmail.com>
2023-01-03 15:48:43 -0600

    first-commit

---
+++ shopping cart/products.js
@@ -0,0 +1,57 @@
+const products = [
+  {
+    id: 0,
+    name: "Cookie 1",
+    price: 29.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/img2.jpeg",
+  },
+  {
+    id: 1,
+    name: "Cookie 2",
+    price: 24.99,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/img2.jpeg",
+  },
+  {
+    id: 2,
+    name: "Cookie 3",
+    price: 19.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/img3.jpeg",
+  },
+  {
+    id: 3,
+    name: "Cake 1",
+    price: 25.99,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/img4.jpeg",
+  },
+  {
+    id: 4,
+    name: "Cake 2",
+    price: 29.99,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/img5.jpeg",
+  },
+  {
+    id: 5,
+    name: "Cake 3",
+    price: 39.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/img6.jpeg",
+  },
+];
+
